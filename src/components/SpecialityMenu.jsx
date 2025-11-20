cea9d4204f903c38d71170749388c682a30a2b2f
catherin-raichal-i <raichal.catherin@gmail.com>
2025-11-20 12:19:06 +0530

    Update SpecialityMenu.jsx

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -6,12 +6,10 @@ const SpecialityMenu = () => {
   return (
     <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
       <h1 className='text-3xl font-medium'>Find by Speciality</h1>
-      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free</p>
-
+      <p className='sm:w-1/3 text-center text-sm'>Browse our trusted doctors by specialty and book your appointment quickly and hassle-free.</p>
+    
       <div className='flex sm:justify-center gap-4 pt-5 w-full  overflow-scroll'>
-        {/* overflow-scroll */}
-        {/* overflow-auto[&::-webkit-scrollbar-width:hidden] */}
-        {/* [-ms-overflow-style:none],[&::-webkit-scrollbar]:hidden */}
+        
         {specialityData.map((item, index) => (
           <Link onClick={() => scrollTo(0, 0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
             <img className='w-16 sm:w-24 mb-2' src={item.image} alt="" />
@@ -22,5 +20,6 @@ const SpecialityMenu = () => {
     </div>
   )
 }
+
 
 export default SpecialityMenu
