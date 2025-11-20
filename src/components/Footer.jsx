d8a37232f8896f19be21b9fa95059d8fcafc3f06
catherin-raichal-i <raichal.catherin@gmail.com>
2025-11-20 13:47:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
                 <div>
                     <p className='text-xl font-medium mb-5'>Get Touch</p>
                     <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>+91-895-568-25-85</li>
+                        <li>+91 98765 43210</li>
                         <li>prescripto@gmail.com</li>
                     </ul>
                 </div>
@@ -49,3 +49,4 @@ const Footer = () => {
 
 
 export default Footer
+
